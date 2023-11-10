cc2b8e782e1c3e888b456ad663dda24a4b2bfd95
Nicolas Menard <nicolas.p.menard@gmail.com>
2023-11-10 11:00:01 -0500

    Update python script

--- hook.py
+++ hook.py
@@ -1,13 +1,14 @@
 import asyncio
 import sys
-import time
-import logging
 from datetime import timedelta
 
 from viam.robot.client import RobotClient
 from viam.components.board import Board
 from viam.rpc.dial import Credentials, DialOptions
 from viam.proto.component.board import PowerMode
+from viam.logging import getLogger
+
+LOGGER = getLogger("example-script")
 
 async def connect(location, secret):
     creds = Credentials(
@@ -16,53 +17,50 @@ async def connect(location, secret):
     opts = RobotClient.Options(
         refresh_interval=0,
         dial_options=DialOptions(credentials=creds),
-        attempt_reconnect_interval=15,
+        check_connection_interval=0,
+        attempt_reconnect_interval=0,
         disable_sessions=True
     )
     return await RobotClient.at_address(location, opts)
 
 async def main():
-    location = sys.argv[1]
+    robot_url = sys.argv[1]
     secret = sys.argv[2]
-    print(f"location: {location}")
-    print(f"secret: {secret}")
-    time.sleep(3)
 
     # duration of deep sleep until device's next call to webhook
     delta = timedelta(
-        #days=50,
-        #seconds=10,
-        #microseconds=10,
-        #milliseconds=29000,
         minutes=1,
-        #hours=8,
-        #weeks=2
     )
 
-    while True:
+    # sometimes the connection can fail, we want to try a couple of time before giving up
+    n_try = 10
+    while n_try:
         try:
-            robot = await connect(location, secret)
-            board = Board.from_robot(robot, "board")
-            gpio = await board.gpio_pin_by_name("15")
-            await gpio.set(True)
-            time.sleep(3)
-            await gpio.set(False)
-
-            # The final method makes a call that expects a response in order to return properly,
-            # however, if the power is successfully set to Deep Sleep, the device will not be able
-            # to respond, throwing an exception.
-            try: 
-                await board.set_power_mode(PowerMode.POWER_MODE_OFFLINE_DEEP, duration=delta)
-            except Exception as _:
-                logging.info("success!")
-                return
+            robot = await connect(robot_url,secret)
+            break;
         except Exception as e:
-            logging.error(e)
+            n_try = n_try - 1;
+            LOGGER.info("couldn't connect to robot reason {} trying again for {} times".format(e, n_try));
+            if n_try == 0:
+                LOGGER.error("couldn't connect to robot after 10 tries giving up")
+                return
 
-    # As the connection would time out if successfully put to deep sleep, `robot.close()`
-    # is not necessary and would fail.
+    LOGGER.info(f"Resources {robot.resource_names}")
+
+    try:
+        await board.set_power_mode(PowerMode.POWER_MODE_OFFLINE_DEEP, duration=delta, timeout = 5)
+    except:
+        LOGGER.info("set power mode doesn't return assuming everything went well")
+
+    # The ESP32 is sleeping at this point so we aren't able to gracefully close the connection
+    # we therefore quit immediately
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    sys.exit()
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        LOGGER.info(f"closing script after exception {e}")
+    sys.exit(0)
  
